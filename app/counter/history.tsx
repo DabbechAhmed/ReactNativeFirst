674823699b4c2baaa6c558f5c7f8da93b2abf786
DabbechAhmed <ahmed.dabbech@ieee.org>
2024-11-25 00:15:13 +0100

    Completed my work in this react native project i loved it git add .

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,10 +1,48 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { countdownStorageKey, PersistedCountDownState } from ".";
+import { getFromStorage } from "../../utils/storage";
+import { format } from "date-fns";
+import { theme } from "../../theme";
+
+const fullDateFormat = `LLL d yyyy, h:mm aaa`;
 
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] = useState<PersistedCountDownState>(
+    {
+      currentNotificationId: undefined,
+      completedAtTimestamp: [],
+    }
+  );
+  useEffect(() => {
+    const loadState = async () => {
+      const state = await getFromStorage(countdownStorageKey);
+      if (state) {
+        setCountdownState(state);
+      }
+    };
+    loadState();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      data={countdownState?.completedAtTimestamp}
+      keyExtractor={(item) => item.toString()}
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.listItemText}>
+            {format(item, fullDateFormat)}
+          </Text>
+        </View>
+      )}
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      ListEmptyComponent={
+        <View style={styles.listEmpty}>
+          <Text style={styles.listEmptyText}>No items</Text>
+        </View>
+      }
+    />
   );
 }
 
@@ -16,5 +54,32 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    marginTop: 8,
+  },
+  listItem: {
+    padding: 12,
+    marginBottom: 8,
+    backgroundColor: theme.colorLightGrey,
+    marginHorizontal: 8,
+    borderRadius: 6,
+  },
+  listItemText: {
+    fontSize: 16,
+  },
+  listEmpty: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 18,
+  },
+  listEmptyText: {
+    fontSize: 24,
+    color: theme.colorGrey,
   },
 });
--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,196 @@
-import { useRouter } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator,
+  Dimensions,
+  useWindowDimensions,
+} from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { useEffect, useRef, useState } from "react";
+import { Duration, intervalToDuration, isBefore, set } from "date-fns";
+import { TimeSegment } from "../../components/TimeSegment";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { saveToStorage } from "../../utils/storage";
+import * as Haptics from "expo-haptics";
+import ConfettiCannon from "react-native-confetti-cannon";
+
+const frequency = 14 * 24 * 60 * 60 * 1000;
+
+export const countdownStorageKey = "countdown";
+
+type CountDownStatus = {
+  isOverdue: boolean;
+  distance: Duration;
+};
+export type PersistedCountDownState = {
+  currentNotificationId: string | undefined;
+  completedAtTimestamp: number[];
+};
 
 export default function CounterScreen() {
+  const { width } = useWindowDimensions();
+  const confettiRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+  const [countdownState, setCountdownState] = useState<PersistedCountDownState>(
+    {
+      currentNotificationId: undefined,
+      completedAtTimestamp: [],
+    }
+  );
+  const [status, setStatus] = useState<CountDownStatus>({
+    isOverdue: false,
+    distance: {},
+  });
+
+  const lastCompletedTimestamp = countdownState?.completedAtTimestamp[0];
+
+  useEffect(() => {
+    const loadState = async () => {
+      const state = await AsyncStorage.getItem(countdownStorageKey);
+      if (state) {
+        setCountdownState(JSON.parse(state));
+      }
+    };
+    loadState();
+  }, []);
+  const scheduleNotification = async () => {
+    confettiRef?.current?.start();
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    let pushNotificationId;
+    const result = await registerForPushNotificationsAsync(); // This is the function we want to test
+    console.log(result);
+
+    if (result === "granted") {
+      pushNotificationId = await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "notification from the future 🚀 ",
+          body: "Don't forget to buy groceries!",
+        },
+        trigger: {
+          type: "timeInterval",
+          seconds: frequency / 1000,
+        },
+      });
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notification",
+          "Enable notifications in your device settings to receive reminders"
+        );
+      }
+    }
+    if (countdownState?.currentNotificationId) {
+      await Notifications.cancelScheduledNotificationAsync(
+        countdownState.currentNotificationId
+      );
+    }
+
+    const newCountdownState: PersistedCountDownState = {
+      currentNotificationId: pushNotificationId,
+      completedAtTimestamp: countdownState
+        ? [Date.now(), ...countdownState.completedAtTimestamp]
+        : [Date.now()],
+    };
+    setCountdownState(newCountdownState);
+    await saveToStorage(countdownStorageKey, newCountdownState);
+  };
+  // console.log(status);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+    // Set notification handler
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+      }),
+    });
+  }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const timestamp = lastCompletedTimestamp
+        ? lastCompletedTimestamp + frequency
+        : Date.now();
+      if (lastCompletedTimestamp) {
+        setLoading(false);
+      }
+
+      const isOverdue = isBefore(timestamp, Date.now());
+      const distance = intervalToDuration(
+        isOverdue
+          ? { start: timestamp, end: Date.now() }
+          : { start: Date.now(), end: timestamp }
+      );
+      setStatus({ isOverdue, distance });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [lastCompletedTimestamp]);
+
+  if (loading) {
+    return (
+      <View style={styles.activityIndicatorContainer}>
+        <ActivityIndicator size="large" color={theme.colorBlack} />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+    <View
+      style={[
+        styles.container,
+        status.isOverdue ? styles.containerLate : undefined,
+      ]}
+    >
+      {status.isOverdue ? (
+        <Text style={[styles.heading, styles.whiteText]}>
+          Car wash overdue by
+        </Text>
+      ) : (
+        <Text style={styles.heading}>Car wash due in...</Text>
+      )}
+      <View style={styles.row}>
+        <TimeSegment
+          unit="Days"
+          number={status.distance.days ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={status.distance.hours ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={scheduleNotification}
+      >
+        <Text style={styles.buttonText}>I've washed the car!</Text>
+      </TouchableOpacity>
+      <ConfettiCannon
+        count={50}
+        origin={{ x: width / 2, y: -20 }}
+        ref={confettiRef}
+        autoStart={false}
+        fadeOut
+      />
     </View>
   );
 }
@@ -16,7 +202,38 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
   },
-  text: {
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    fontSize: 20,
+    textTransform: "uppercase",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 24,
+  },
+  heading: {
     fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  containerLate: {
+    backgroundColor: theme.colorRed,
+  },
+  whiteText: {
+    color: theme.colorWhite,
+  },
+  activityIndicatorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colorWhite,
   },
 });
